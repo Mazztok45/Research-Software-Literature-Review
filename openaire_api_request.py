8ac9bd84f4c2c766f0b24981cc968054f3905ae6
Mazztok45 <maxence.azzouz@gmail.com>
2025-04-04 16:45:27 +0200

    modfying requests parameters for openaire search endpoint

--- openaire_api_request.py
+++ openaire_api_request.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.openaire.eu/search/publications"
 params = {
-    "title": "research software OR software citation OR metadata",
+    "keywords": "research software",
     "size": 200,
     "page": 1,
     "format": "json"
@@ -15,6 +15,6 @@ response = requests.get(url, headers=headers, params=params)
 
 if response.status_code == 200:
     data = response.json()
-    print(data)
+    print(data["response"]["results"]["result"][0]["metadata"]["oaf:entity"]["oaf:result"]["pid"])
 else:
     print(f"Failed to retrieve data: {response.status_code}")
